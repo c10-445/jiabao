887ba86498eb23a30c8ae42a8af8d58e207ea620
lihuizhang <969388016@qq.com>
2018-04-02 22:56:14 +0800

    try

--- app/src/main/java/edu/jiabao/MainActivity.java
+++ app/src/main/java/edu/jiabao/MainActivity.java
@@ -35,10 +35,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
-
+    public static int x=21;
 }
